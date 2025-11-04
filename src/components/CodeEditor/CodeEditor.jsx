a1cffe4aae2e5a7a8ea3790d665da0fb08f65863
karthik-1867 <>
2025-11-04 13:06:05 +0530

    update

--- src/components/CodeEditor/CodeEditor.jsx
+++ src/components/CodeEditor/CodeEditor.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useCallback } from 'react';
 import CodeMirror from '@uiw/react-codemirror';
 import { EditorState } from '@codemirror/state';
-import { tokyoNightStorm, tokyoNightStormInit } from '@uiw/codemirror-theme-tokyo-night-storm';
+
 import { tomorrowNightBlue } from '@uiw/codemirror-theme-tomorrow-night-blue';
 import { basicSetup } from 'codemirror';  // depends on your wrapper
 import { javascript } from '@codemirror/lang-javascript';
-import { githubLightStyle } from "@uiw/codemirror-theme-github";
 
 const languageExtensions = {
   js: javascript({ jsx: true, typescript: false }),
